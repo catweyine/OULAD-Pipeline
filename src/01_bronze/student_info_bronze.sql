2a48a4c7a559a2bda4b10d0a55e23d78c099d39e
jessc.manuel@gmail.com <jessc.manuel@gmail.com>
2026-09-07 17:09:48 +0000

    Ingestion for Student Info bronze tables

--- src/01_bronze/student_info_bronze.sql
+++ src/01_bronze/student_info_bronze.sql
@@ -1,1 +1,12 @@
+CREATE TABLE IF NOT EXISTS oulad.oulad_bronze.student_info_bronze
+USING DELTA
+AS
+SELECT *,
+       current_timestamp() AS ingestion_timestamp,
+       current_date() AS ingestion_date
+FROM read_files(
+  '/Volumes/workspace/default/ftw-b12-de/shared/week07/studentInfo.csv',
+  format => 'csv',
+  header => true
+);
 
