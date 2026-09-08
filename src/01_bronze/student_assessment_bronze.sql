7b96f8b9b6042149f0e500d7631427aa5a3dc111
Jemma Santiago <jghsantiago91@gmail.com>
2026-09-08 23:19:57 +0800

    Added queries for assessment and student assessment ingestion (bronze) (#10)

--- src/01_bronze/student_assessment_bronze.sql
+++ src/01_bronze/student_assessment_bronze.sql
@@ -1,1 +1,11 @@
-
+CREATE TABLE IF NOT EXISTS oulad.oulad_bronze.student_assessment_bronze
+USING DELTA
+AS
+SELECT *,
+       current_timestamp() AS ingestion_timestamp,
+       current_date() AS ingestion_date
+FROM read_files(
+  '/Volumes/workspace/default/ftw-b12-de/shared/week07/studentAssessment.csv',
+  format => 'csv',
+  header => true
+);
