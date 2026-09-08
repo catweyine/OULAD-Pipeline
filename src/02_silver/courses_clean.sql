b069f5a3e90f423181d3b5c2ff6246ba80efc699
Joan Maquinano <158557104+JoanMaquinano@users.noreply.github.com>
2026-09-08 18:35:50 +0800

    Created courses_clean.sql (#5)
    Co-authored-by: joan.maquinano@ftwfoundation.org <joan.maquinano@ftwfoundation.org>

--- src/02_silver/courses_clean.sql
+++ src/02_silver/courses_clean.sql
@@ -1,1 +1,9 @@
+CREATE OR REPLACE TABLE oulad.oulad_silver.courses_silver AS
+SELECT DISTINCT
+    UPPER(TRIM(code_module)) AS code_module,
+    UPPER(TRIM(code_presentation)) AS code_presentation,
+    CAST(module_presentation_length AS INT) AS module_presentation_length,
+    ingestion_timestamp,
+    ingestion_date
+FROM oulad.oulad_bronze.courses_bronze;
 
