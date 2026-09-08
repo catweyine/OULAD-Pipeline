9a298e46a9b1799cf50f371d800ad653afa2f95f
go-viaaa <viacabuyao@gmail.com>
2026-09-08 23:22:57 +0800

    feature/bronze-studentvle (#16)
    feature/bronze-studentvle
    
    Co-authored-by: via.cabuyao@ftwfoundation.org <via.cabuyao@ftwfoundation.org>

--- src/01_bronze/student_vle_bronze.sql
+++ src/01_bronze/student_vle_bronze.sql
@@ -1,1 +1,11 @@
-
+CREATE TABLE IF NOT EXISTS oulad.oulad_bronze.student_vle_bronze
+USING DELTA
+AS
+SELECT *,
+       current_timestamp() AS ingestion_timestamp,
+       current_date() AS ingestion_date
+FROM read_files(
+  '/Volumes/workspace/default/ftw-b12-de/shared/week07/studentVle.csv',
+  format => 'csv',
+  header => true
+);
