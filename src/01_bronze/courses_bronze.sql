a396b87e940b1b52dae741e113b45c6df89042ba
joan.maquinano@ftwfoundation.org <joan.maquinano@ftwfoundation.org>
2026-09-08 10:00:15 +0000

    Created courses_bronze

--- src/01_bronze/courses_bronze.sql
+++ src/01_bronze/courses_bronze.sql
@@ -1,1 +1,7 @@
-
+CREATE TABLE IF NOT EXISTS oulad.oulad_bronze.courses_bronze
+USING DELTA
+AS
+SELECT *,
+  current_timestamp() AS ingestion_timestamp,
+  current_date() AS ingestion_date
+FROM read_files('/Volumes/instacart/default/ftw_b12_de/shared/week07/courses.csv');
