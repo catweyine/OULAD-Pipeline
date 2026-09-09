c680fc0ba6a56f03bb9c8f99696df97c55edf945
jghsantiago91@gmail.com <jghsantiago91@gmail.com>
2026-09-09 12:33:39 +0000

    fact table (fact_student_performance)+dim_assessment

--- src/03_gold/fact_table_gold.sql
+++ src/03_gold/fact_table_gold.sql
@@ -1,1 +1,13 @@
+USE CATALOG oulad;
 
+CREATE OR REPLACE TABLE oulad.oulad_gold.fact_student_performace 
+USING DELTA
+AS
+SELECT sp.id_student, sp.id_assessment, a.code_module, a.code_presentation,
+sp.date_submitted, sp.is_banked, sp.score,
+CASE WHEN a.date IS NOT NULL THEN sp.date_submitted - a.date END AS days_late,
+    sp.score IS NULL  AS is_non_submission,
+    CASE WHEN sp.score IS NOT NULL THEN sp.score * a.weight / 100.0 END AS weighted_contribution 
+FROM oulad.oulad_silver.student_assessment_silver AS sp
+INNER JOIN oulad.oulad_silver.assessments_silver AS a
+ON sp.id_assessment =a.id_assessment;
