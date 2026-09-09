e2c03705c67b51531ce47ae49f6d383329cc3f98
go-viaaa <viacabuyao@gmail.com>
2026-09-09 10:27:28 +0800

    feature/student_vle_silver (#20)
    feature/student_vle_silver
    
    Co-authored-by: via.cabuyao@ftwfoundation.org <via.cabuyao@ftwfoundation.org>

--- src/02_silver/student_vle_clean.sql
+++ src/02_silver/student_vle_clean.sql
@@ -1,1 +1,66 @@
-
+SELECT *
+FROM oulad.oulad_bronze.student_vle_bronze
+LIMIT 10;
+ 
+-- Create the clean (silver) table for student_vle
+CREATE TABLE IF NOT EXISTS oulad.oulad_silver.student_vle_silver (
+    code_module        STRING,
+    code_presentation   STRING,
+    id_student           INT,
+    id_site              INT,
+    date                 INT,
+    sum_click            INT,
+    ingestion_timestamp  TIMESTAMP,
+    ingestion_date       DATE,
+    CONSTRAINT student_vle_silver_pk PRIMARY KEY (code_module, code_presentation, id_student, id_site, date)
+)
+USING DELTA;
+ 
+-- Merge cleaned data from bronze to capture idempotency
+MERGE INTO oulad.oulad_silver.student_vle_silver AS target
+USING (
+    SELECT
+        UPPER(TRIM(code_module))       AS code_module,
+        UPPER(TRIM(code_presentation)) AS code_presentation,
+        id_student,
+        id_site,
+        date,
+        sum_click,
+        ingestion_timestamp,
+        CAST(ingestion_timestamp AS DATE) AS ingestion_date
+    FROM (
+        SELECT *,
+               ROW_NUMBER() OVER (
+                   PARTITION BY code_module, code_presentation, id_student, id_site, date
+                   ORDER BY ingestion_timestamp DESC
+               ) AS row_num
+        FROM oulad.oulad_bronze.student_vle_bronze
+        WHERE code_module IS NOT NULL
+          AND code_presentation IS NOT NULL
+          AND id_student IS NOT NULL
+          AND id_site IS NOT NULL
+          AND date IS NOT NULL
+          AND sum_click > 0
+          AND ingestion_timestamp IS NOT NULL
+    )
+    WHERE row_num = 1
+) AS source
+ON  target.code_module = source.code_module
+AND target.code_presentation = source.code_presentation
+AND target.id_student = source.id_student
+AND target.id_site = source.id_site
+AND target.date = source.date
+WHEN MATCHED THEN
+    UPDATE SET
+        target.sum_click           = source.sum_click,
+        target.ingestion_timestamp = source.ingestion_timestamp,
+        target.ingestion_date      = source.ingestion_date
+WHEN NOT MATCHED THEN
+    INSERT (code_module, code_presentation, id_student, id_site, date, sum_click, ingestion_timestamp, ingestion_date)
+    VALUES (source.code_module, source.code_presentation, source.id_student, source.id_site, source.date, source.sum_click, source.ingestion_timestamp, source.ingestion_date);
+ 
+-- Check the table
+SELECT *
+FROM oulad.oulad_silver.student_vle_silver
+LIMIT 10;
+ 
