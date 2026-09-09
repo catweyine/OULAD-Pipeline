09a6844aa7503ea2ee24b4e6e409eea0f877baf3
via.cabuyao@ftwfoundation.org <via.cabuyao@ftwfoundation.org>
2026-09-09 13:09:10 +0000

    gold.fact_student_vle_engagement
    gold.fact_student_vle_engagement

--- src/03_gold/fact_table_gold.sql
+++ src/03_gold/fact_table_gold.sql
@@ -1,1 +1,19 @@
-
+CREATE OR REPLACE TABLE oulad.oulad_gold.fact_student_vle_engagement
+USING DELTA
+AS
+SELECT
+    sv.id_student,
+    sv.id_site,
+    sv.code_module,
+    sv.code_presentation,
+    sv.date            AS interaction_date,
+    sv.sum_click        AS click_count,
+    dv.activity_type,
+    dv.week_from,
+    dv.week_to
+FROM oulad.oulad_silver.student_vle_clean AS sv
+INNER JOIN oulad.oulad_gold.dim_vle AS dv
+    ON sv.id_site = dv.id_site
+INNER JOIN oulad.oulad_gold.dim_course AS dc
+    ON sv.code_module = dc.code_module
+   AND sv.code_presentation = dc.code_presentation;
